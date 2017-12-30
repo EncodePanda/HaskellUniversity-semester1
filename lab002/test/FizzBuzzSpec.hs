3598ff9a90da6e4734f580d001df4cfb57d735b8
Pawel Szulc <paul.szulc@gmail.com>
2017-12-31 00:39:27 +0100

    Add second property to FizzBuzz

--- lab002/test/FizzBuzzSpec.hs
+++ lab002/test/FizzBuzzSpec.hs
@@ -1,5 +1,9 @@
 module FizzBuzzSpec where
 
+
+import Data.Maybe
+import Text.Read
+
 import Test.Hspec
 import Test.QuickCheck
 import FizzBuzz
@@ -12,3 +16,12 @@ spec :: Spec
 spec = describe "FizzBuzz" $ do
   it "generated list is of size n bla" $ do
     property (\(Positive n) -> (length $ fizzbuzz n) == n)
+
+  it "has only numbers or Fizz or Buzz" $ do
+    property (\(Positive n) -> null $ filter (not . allowed) (fizzbuzz n))
+
+
+allowed :: String -> Bool
+allowed "Fizz" = True
+allowed "Buzz" = True
+allowed str = isJust ((readMaybe str)::(Maybe Int))
