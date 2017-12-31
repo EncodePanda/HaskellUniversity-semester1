9a7a211fa944951ae2cc0c64dd79ed920b4761c1
Pawel Szulc <paul.szulc@gmail.com>
2017-12-31 01:04:26 +0100

    Extends second property for FizzBuzz and provide solution

--- lab002/src/FizzBuzz.hs
+++ lab002/src/FizzBuzz.hs
@@ -1,5 +1,15 @@
 module FizzBuzz(fizzbuzz) where
 
-fizzbuzz :: Int -> [String]
-fizzbuzz n = map (\i -> show i) [1..n] 
+import Data.List
 
+fizzbuzz :: Int -> [String]
+fizzbuzz n = take n (zipWith pick fizzbuzzes numbers)
+  where
+    numbers = map show [1..]
+    fizzes  = cycle ["", "", "Fizz"]
+    buzzes  = cycle ["", "", "", "", "Buzz"]
+    fizzbuzzes = zipWith (++) fizzes buzzes
+
+    pick :: String -> String -> String
+    pick "" n = n
+    pick w _  = w
--- lab002/test/FizzBuzzSpec.hs
+++ lab002/test/FizzBuzzSpec.hs
@@ -17,11 +17,17 @@ spec = describe "FizzBuzz" $ do
   it "generated list is of size n bla" $ do
     property (\(Positive n) -> (length $ fizzbuzz n) == n)
 
-  it "has only numbers or Fizz or Buzz" $ do
+  it "has only allowed numbers (not divide by 3 o 5) or Fizz or Buzz" $ do
     property (\(Positive n) -> null $ filter (not . allowed) (fizzbuzz n))
-
 
 allowed :: String -> Bool
 allowed "Fizz" = True
 allowed "Buzz" = True
-allowed str = isJust ((readMaybe str)::(Maybe Int))
+allowed "FizzBuzz" = True
+allowed str = isJust $ ((readMaybe str)::(Maybe Int)) >>= (\n -> allowedInt n)
+  where
+    allowedInt :: Int -> Maybe Int
+    allowedInt n
+      | n `mod` 3 == 0 = Nothing
+      | n `mod` 5 == 0 = Nothing
+      | otherwise      = Just n
